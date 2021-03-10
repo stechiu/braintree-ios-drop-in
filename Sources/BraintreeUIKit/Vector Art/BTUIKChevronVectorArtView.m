1eeed0f13cca5e3248345862b290426531582f71
Susan Stevens <susan.stevens@getbraintree.com>
2021-03-10 15:55:52 -0600

    Update Chevron to point left (or right) depending on direction of language
    Signed-off-by: Samantha Cannillo <samantha.cannillo@getbraintree.com>

--- Sources/BraintreeUIKit/Vector Art/BTUIKChevronVectorArtView.m
+++ Sources/BraintreeUIKit/Vector Art/BTUIKChevronVectorArtView.m
@@ -1,9 +1,11 @@
-#import "BTUIKChevronVectorArtView.h"
-
 #ifdef COCOAPODS
 #import <BraintreeDropIn/BTUIKAppearance.h>
+#import <BraintreeDropIn/BTUIKChevronVectorArtView.h>
+#import <BraintreeDropIn/BTUIKViewUtil.h>
 #else
 #import <BraintreeUIKit/BTUIKAppearance.h>
+#import <BraintreeUIKit/BTUIKChevronVectorArtView.h>
+#import <BraintreeUIKit/BTUIKViewUtil.h>
 #endif
 
 @implementation BTUIKChevronVectorArtView
@@ -11,7 +13,7 @@
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.artDimensions = CGSizeMake(45.0f, 29.0f);
+        self.artDimensions = CGSizeMake(40.0f, 40.0f);
         self.opaque = NO;
     }
     return self;
@@ -20,13 +22,25 @@
 - (void)drawArt {
     //// Bezier Drawing
     UIBezierPath* bezierPath = [UIBezierPath bezierPath];
-    [bezierPath moveToPoint: CGPointMake(15.5, 12.21)];
-    [bezierPath addLineToPoint: CGPointMake(30.07, 25.07)];
-    [bezierPath addLineToPoint: CGPointMake(15.5, 38.79)];
-    [bezierPath addLineToPoint: CGPointMake(17.21, 40.5)];
-    [bezierPath addLineToPoint: CGPointMake(33.5, 25.07)];
-    [bezierPath addLineToPoint: CGPointMake(17.21, 10.5)];
-    [bezierPath addLineToPoint: CGPointMake(15.5, 12.21)];
+
+    if (BTUIKViewUtil.isLanguageLayoutDirectionRightToLeft) {
+        [bezierPath moveToPoint: CGPointMake(34, 11.71)];
+        [bezierPath addLineToPoint: CGPointMake(19.43, 24.57)];
+        [bezierPath addLineToPoint: CGPointMake(34, 38.29)];
+        [bezierPath addLineToPoint: CGPointMake(32.29, 40)];
+        [bezierPath addLineToPoint: CGPointMake(16, 24.57)];
+        [bezierPath addLineToPoint: CGPointMake(32.29, 10)];
+        [bezierPath addLineToPoint: CGPointMake(34, 11.71)];
+    } else {
+        [bezierPath moveToPoint: CGPointMake(15.5, 12.21)];
+        [bezierPath addLineToPoint: CGPointMake(30.07, 25.07)];
+        [bezierPath addLineToPoint: CGPointMake(15.5, 38.79)];
+        [bezierPath addLineToPoint: CGPointMake(17.21, 40.5)];
+        [bezierPath addLineToPoint: CGPointMake(33.5, 25.07)];
+        [bezierPath addLineToPoint: CGPointMake(17.21, 10.5)];
+        [bezierPath addLineToPoint: CGPointMake(15.5, 12.21)];
+    }
+
     [bezierPath closePath];
     [BTUIKAppearance.sharedInstance.lineColor setFill];
     [bezierPath fill];
